d2b0b9159d95ababc3b745cc977f0410b7f2647d
Akshayadandu <Akshayadandu2003a@gmail.com>
2025-12-17 00:01:20 +0530

    Add files via upload

---
+++ MovieTkt/Shows.java
@@ -0,0 +1,32 @@
+package MovieTkt;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ShowSelectionServlet")
+public class Shows extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Shows() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		RequestDispatcher rd = request.getRequestDispatcher("Shows.html");
+        rd.forward(request, response);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
